114fdab7bdc896351208f29b6b722cd626a63f10
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-07-09 20:04:46 -0400

    Update Package JSON for React and About Page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function About (){
-    
+    // Biography About Ourselve 
+    return (
+        <div>
+            <p>Appraiser with the intentions of becoming Creative creator as Front End Developer (Full Stack Developer)</p>
+            <br></br>
+            <img src="/" alt= "Photo of Myself" />
+            <p>
+                Hello! My name is Tyler and I am currently a Real Estate Appraiser during the daytime, while I am going to school at nighttime to pursue a different career in the web development field be showcase my creativity skills.
+            </p>
+            <p>
+                Self-starter motivated individual who retains information with hands on experience. I am adaptable and willing to take outside of my comfort zone to learn new skills. 
+            </p>
+        </div>
+    )
 }
 
 
